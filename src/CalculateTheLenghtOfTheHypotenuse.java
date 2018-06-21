d7d1fd022489615973083547c5b3fffc534009cd
Andrey Sokolov <a.sokolov@hh.ru>
2018-06-21 14:30:08 +0300

    LSS-1 Scanner update

--- src/CalculateTheLenghtOfTheHypotenuse.java
+++ src/CalculateTheLenghtOfTheHypotenuse.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class CalculateTheLenghtOfTheHypotenuse {
 
   public static void main(String[] args) {
     TheLegsAndTheHypotenuse Hypotenuse = new TheLegsAndTheHypotenuse();
-    Hypotenuse.a = 2;
-    Hypotenuse.b = 3;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Введите размеры катетов: ");
+    Hypotenuse.a = scan.nextInt();
+    Hypotenuse.b = scan.nextInt();
     Hypotenuse.c = Math.sqrt(Math.pow(Hypotenuse.a, 2) + Math.pow(Hypotenuse.b, 2));
     System.out.print("Гипотенуза равна " + Hypotenuse.c);
   }
